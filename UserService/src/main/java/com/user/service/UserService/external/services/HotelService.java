0e838aef9c61de65f32b7bc779077e405393bb7d
Neo <nirajkumarnirala633@gmail.com>
2025-03-09 23:18:25 +0530

    feign-client

---
+++ UserService/src/main/java/com/user/service/UserService/external/services/HotelService.java
@@ -0,0 +1,15 @@
+package com.user.service.UserService.external.services;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.user.service.UserService.entities.Hotel;
+
+
+@FeignClient(name = "HOTELSERVICE")
+public interface HotelService {
+    
+    @GetMapping("/hotels/{hotelId}")
+    Hotel getHotel(@PathVariable("hotelId") String hotelId);
+}
--- UserService/src/main/java/com/user/service/UserService/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/user/service/UserService/services/impl/UserServiceImpl.java
@@ -1,22 +1,36 @@
 package com.user.service.UserService.services.impl;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import com.user.service.UserService.entities.Hotel;
+import com.user.service.UserService.entities.Rating;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.user.service.UserService.UserService;
 import com.user.service.UserService.entities.User;
 import com.user.service.UserService.exceptions.ResourceNotFoundException;
+import com.user.service.UserService.external.services.HotelService;
 import com.user.service.UserService.repository.UserRepository;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private HotelService hotelService;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User saveUser(User user) {
@@ -34,7 +48,34 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server !! : "+userId));
+        User user =  userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server !! : "+userId));
+
+        Rating[] ratingsOfUser = restTemplate.getForObject("http://RATINGSERVICE/ratings/users/"+user.getUserId(), Rating[].class);
+
+        logger.info("{}",ratingsOfUser);
+
+        List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
+
+
+        List<Rating> ratingList = ratings.stream().map(rating -> {
+//            api call to hotel service to get the hotel
+//            http://localhost:8081/hotels/ccb3eea5-8307-4614-95dd-94e8a3a825a2
+            // ResponseEntity<Hotel> forEntity =  restTemplate.getForEntity("http://HOTELSERVICE/hotels/"+rating.getHotelId(), Hotel.class);
+            // Hotel hotel = forEntity.getBody();
+
+            Hotel hotel = hotelService.getHotel(rating.getHotelId());
+            // logger.info("response status code: {}",forEntity.getStatusCode());
+
+//            set the hotel to rating
+            rating.setHotel(hotel);
+
+//            return the rating
+            return  rating;
+        }).collect(Collectors.toList());
+
+        user.setRatings(ratingList);
+
+        return user;
     }
 
     @Override
